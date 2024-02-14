695d6133391c2ae13322b43aecf4fff07bf0b2ad
Sharion2023 <Sharion@gmail.com>
2024-02-14 16:48:48 -0600

    Added individual delete options.

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
 
-  const[people, setPeople] = useState(data)
+  //const[people, setPeople] = useState(items)
 
   return <main>
     <section className='container'>
-    <h3>{people.length} birthdays today</h3>
-    <List people={people}/>
-    <button onClick={() => setPeople([])}> clear all</button>
-
+   
+    <h3>{List.length} birthdays today</h3>
+    <List/>
+    
     </section>
   </main>;
 }
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import data from './data';
 
-const List = ({people}) => {
+
+
+const List = () => {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(()=> {
+    setItems(data);
+  }, [])
+
+  const deleteItem = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    console.log(id);
+    alert("Delete button engaged!")
+    setItems(newItems)
+  }
+  
   return (
     <>
-      {people.map((person)=> {
+      {items.map((person)=> {
         const {id, name, age, image} = person;
+        //console.log(id)
         return (
         <article key={id} className="person">
-          <img src={image} alt={name} />
+          <img src={image} alt={name} /> <button onClick={() => deleteItem(id)} >click to remove</button>
           <div>
             <h4>{name}</h4>
             <p>{age} years</p>
           </div>
         </article>
+       
+
         );
       })};
+      <button onClick={() => setItems([])}> clear all</button>
     </>
   );
 };
