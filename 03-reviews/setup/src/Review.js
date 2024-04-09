d51e671b147ff73ef9d6531e86101a2e6f3fd062
Sharion2023 <Sharion@gmail.com>
2024-04-09 12:52:38 -0500

    Project 3 started

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,14 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0); 
+  const {name, job, image, text} = people[index]
+  
+  return <article className='review'>
+  <div className='img-container'>
+    <img src={image} alt={text} />
+  </div>
+    </article>;
 };
 
 export default Review;
