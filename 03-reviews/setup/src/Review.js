0571549182c85236f604e0171940d5aee95aec40
Sharion2023 <Sharion@gmail.com>
2024-04-10 17:29:09 -0500

    Random button implemented

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -5,6 +5,7 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0); 
   const {name, job, image, text} = people[index]
+  console.log(index)
   
   return (
   <article className='review'>
@@ -18,15 +19,17 @@ const Review = () => {
     <p className='job'>{job}</p>
     <p className='info'> {text}</p>
     <div className='button-container'>
-      <button className='prev-btn' onClick={()=> (index-1 < 0 ? setIndex(index): setIndex(index-1))}>
+      <button className='prev-btn' onClick={()=> (index-1 < 0 ? setIndex(people.length-1): setIndex(index-1))}>
         <FaChevronLeft/>
       </button>
-      <button className='next-btn' onClick={()=> (index+1 > people.length-1 ? setIndex(index): setIndex(index+1))}>
+      <button className='next-btn' onClick={()=> (index+1 > people.length-1 ? setIndex(0): setIndex(index+1))}>
         <FaChevronRight/>
       </button>
     </div>
+    <button className='random-btn' onClick={()=> setIndex(Math.floor(Math.random()* people.length))}>surprise me!</button>
     
   </article>
+  
   );
 };
 
