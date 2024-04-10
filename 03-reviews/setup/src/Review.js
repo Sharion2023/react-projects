6a4dbe72a5381141cb318d85a5745e672d3319f6
Sharion2023 <Sharion@gmail.com>
2024-04-10 17:11:53 -0500

    Next and previous person functional

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -6,11 +6,28 @@ const Review = () => {
   const [index, setIndex] = useState(0); 
   const {name, job, image, text} = people[index]
   
-  return <article className='review'>
-  <div className='img-container'>
-    <img src={image} alt={text} />
-  </div>
-    </article>;
+  return (
+  <article className='review'>
+    <div className='img-container'>
+      <img src={image} alt={name} className='person-img' />
+      <span className='quote-icon'>
+        <FaQuoteRight />
+      </span>
+    </div>
+    <h4 className='author'>{name}</h4>
+    <p className='job'>{job}</p>
+    <p className='info'> {text}</p>
+    <div className='button-container'>
+      <button className='prev-btn' onClick={()=> (index-1 < 0 ? setIndex(index): setIndex(index-1))}>
+        <FaChevronLeft/>
+      </button>
+      <button className='next-btn' onClick={()=> (index+1 > people.length-1 ? setIndex(index): setIndex(index+1))}>
+        <FaChevronRight/>
+      </button>
+    </div>
+    
+  </article>
+  );
 };
 
 export default Review;
